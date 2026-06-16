4227fed4ac9a5bffa599f27f3364134257033ff3
qwertyzxxxxx <113626485+qwertyzxxxxx@users.noreply.github.com>
2026-06-16 20:02:41 +0700

    feat(performance-center): add __init__.py

---
+++ src/binance_ai_trader/performance_center/__init__.py
@@ -0,0 +1,4 @@
+from .models import StrategyResult, StrategyStats, Leaderboard
+from .center import PerformanceCenter
+
+__all__ = ["StrategyResult", "StrategyStats", "Leaderboard", "PerformanceCenter"]
